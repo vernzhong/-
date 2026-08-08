39c5dd9e0ef76f0755f72953113b341d52092430
vernzhong <404250327@qq.com>
2026-08-08 10:54:07 +0800

    Refactor page.tsx for consistent formatting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,18 @@
-"use client"; 
-"" 
-"import { useState } from 'react';" 
-"import Dashboard from './components/Dashboard';" 
-"import Sidebar from './components/Sidebar';" 
-"" 
-"export default function Home() {" 
-"  return (" 
-"    <div className=\"flex min-h-screen\">" 
-"      <Sidebar />" 
-"      <main className=\"flex-1 flex flex-col\">" 
-"        <div className=\"flex-1 p-6\">" 
-"          <Dashboard />" 
-"        </div>" 
-"      </main>" 
-"    </div>" 
-"  );" 
-"}" 
+"use client";
+
+import { useState } from 'react';
+import Dashboard from './components/Dashboard';
+import Sidebar from './components/Sidebar';
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen">
+      <Sidebar />
+      <main className="flex-1 flex flex-col">
+        <div className="flex-1 p-6">
+          <Dashboard />
+        </div>
+      </main>
+    </div>
+  );
+}
